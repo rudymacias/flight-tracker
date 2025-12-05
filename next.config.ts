929089e73ba132e1d3a4e393fb7d0d2f2860ffdd
Rudy Macias <rudy@creekdigital.co>
2025-12-05 15:22:54 -0600

    Github Pages Setup

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/flight-tracker",
+  assetPrefix: "/flight-tracker",
   output: "export",
   distDir: "build",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
